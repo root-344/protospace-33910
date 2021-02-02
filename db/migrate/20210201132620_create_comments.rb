783560238b0bb4f965522bcef268febca8bc4aab
root-344 <michimasa8@gmail.com>
2021-02-02 18:16:39 +0900

    userテーブルまとめる

--- db/migrate/20210201132620_create_comments.rb
+++ db/migrate/20210201132620_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.references :user_id,      foreign_key: true
-      t.references :prototype_id, foreign_key: true
+      t.references :user,      foreign_key: true
+      t.references :prototype, foreign_key: true
       t.text :text,               null: false
       t.timestamps
     end
